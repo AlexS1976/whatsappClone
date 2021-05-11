16d142d02218c4bdbd65fe39c4e4c92bedc40c54
Alex Silva <damasioesilva@gmail.com>
2021-05-10 21:45:26 -0300

    add user config

--- app/src/main/java/com/example/whatsappclone/activity/MainActivity.java
+++ app/src/main/java/com/example/whatsappclone/activity/MainActivity.java
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 import androidx.viewpager.widget.ViewPager;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuInflater;
@@ -69,6 +70,11 @@ public class MainActivity extends AppCompatActivity {
             case R.id.menuSair:
                 deslogarUsuario();
                 break;
+
+            case R.id.menuConfiguracoes:
+                abrirConfiguracoes();
+
+                break;
         }
         return super.onOptionsItemSelected(item);
     }
@@ -83,5 +89,10 @@ public class MainActivity extends AppCompatActivity {
         }catch (Exception e){
             e.printStackTrace();
         }
+    }
+
+    public void abrirConfiguracoes(){
+        Intent intent = new Intent(MainActivity.this, ConfiguracoesActivity.class);
+        startActivity(intent);
     }
 }
