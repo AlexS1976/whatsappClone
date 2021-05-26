41a0f837013894a66899d2005d501778394eb458
Alex Silva <damasioesilva@gmail.com>
2021-05-25 22:11:14 -0300

    config recyclerView contatos

--- app/src/main/java/com/example/whatsappclone/fragments/ContatosFragment.java
+++ app/src/main/java/com/example/whatsappclone/fragments/ContatosFragment.java
@@ -2,65 +2,101 @@ package com.example.whatsappclone.fragments;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 import com.example.whatsappclone.R;
+import com.example.whatsappclone.adapter.AdapterContatos;
+import com.example.whatsappclone.config.ConfiguracaoFirebase;
+import com.example.whatsappclone.model.Usuario;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.ValueEventListener;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link ContatosFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+
+
 public class ContatosFragment extends Fragment {
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
-
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
+private RecyclerView recyclerViewListaContatos;
+private AdapterContatos adapter;
+private ArrayList<Usuario> listaContatos = new ArrayList<>();
+private DatabaseReference usuariosRef;
+private ValueEventListener valueEventListenerContatos;
 
     public ContatosFragment() {
         // Required empty public constructor
     }
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment ContatosFragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static ContatosFragment newInstance(String param1, String param2) {
-        ContatosFragment fragment = new ContatosFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
-
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
-    }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_contatos, container, false);
+
+        View view = inflater.inflate(R.layout.fragment_contatos, container, false);
+
+        //configuracoes iniciais
+        recyclerViewListaContatos = view.findViewById(R.id.recyclerViewListaContatos);
+        usuariosRef = ConfiguracaoFirebase.getDatabaseReference().child("usuarios");
+
+
+        // confitguracao adapter
+        // instanciar adapter e passar a lista como parametro
+        adapter = new AdapterContatos(listaContatos , getActivity());
+
+
+
+
+        // configurar recycler view
+        //instanciar um gerenciador de layout pegando o contexto da activity
+        RecyclerView.LayoutManager layoutManager = new LinearLayoutManager(getActivity());
+        //define o layout
+        recyclerViewListaContatos.setLayoutManager(layoutManager);
+        //dertermina o numero de elementos visiveis
+        recyclerViewListaContatos.setHasFixedSize(true);
+        //defini o adapter
+        recyclerViewListaContatos.setAdapter(adapter);
+
+        return view;
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        recuperarContatos();
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+        usuariosRef.removeEventListener(valueEventListenerContatos);
+    }
+
+    public void recuperarContatos(){
+       valueEventListenerContatos = usuariosRef.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+
+                for (DataSnapshot dados: snapshot.getChildren()){
+                    Usuario usuario = dados.getValue(Usuario.class);
+                    listaContatos.add(usuario);
+                }
+                adapter.notifyDataSetChanged();
+
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
     }
 }
