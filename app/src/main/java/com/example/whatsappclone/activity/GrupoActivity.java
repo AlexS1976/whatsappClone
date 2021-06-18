448656198c08a793a19d2096a709eab38265e320
Alex Silva <damasioesilva@gmail.com>
2021-06-17 22:23:10 -0300

    recycler membros

--- app/src/main/java/com/example/whatsappclone/activity/GrupoActivity.java
+++ app/src/main/java/com/example/whatsappclone/activity/GrupoActivity.java
@@ -2,18 +2,43 @@ package com.example.whatsappclone.activity;
 
 import android.os.Bundle;
 
+import com.example.whatsappclone.adapter.AdapterContatos;
+import com.example.whatsappclone.config.ConfiguracaoFirebase;
+import com.example.whatsappclone.helper.UsuarioFirebase;
+import com.example.whatsappclone.model.Usuario;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.android.material.snackbar.Snackbar;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.view.View;
 
 import com.example.whatsappclone.R;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class GrupoActivity extends AppCompatActivity {
+
+    private RecyclerView membrosSelecionados, membros;
+    private AdapterContatos adapterContatos;
+    private ArrayList <Usuario> listaMembros = new ArrayList<>();
+    private ValueEventListener valueEventListenerMembros;
+    private FirebaseUser usuarioAtual;
+
+
+    //referencias
+    DatabaseReference usuariosRef;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,13 +51,79 @@ public class GrupoActivity extends AppCompatActivity {
         actionBar.setDisplayHomeAsUpEnabled(true);
 
 
+
+        //configuracao inicial
+
+        membros = findViewById(R.id.membros);
+        membrosSelecionados = findViewById(R.id.membrosSelecionados);
+
+        usuariosRef = ConfiguracaoFirebase.getDatabaseReference().child("usuarios");
+        usuarioAtual = UsuarioFirebase.getUsuarioAtual();
+
+        //configuracao adapter
+
+        adapterContatos = new AdapterContatos(listaMembros, getApplicationContext());
+
+        // configuracao recycler view
+        RecyclerView.LayoutManager layoutManager = new LinearLayoutManager(getApplicationContext());
+        membros.setAdapter(adapterContatos);
+        membros.setLayoutManager(layoutManager);
+
+        membros.setHasFixedSize(true);
+
+
         FloatingActionButton fab = findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
                         .setAction("Action", null).show();
+
             }
         });
+    }
+
+    public void recuperarContatos(){
+
+        valueEventListenerMembros = usuariosRef.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                for (DataSnapshot dados: snapshot.getChildren()){
+                    Usuario usuario = dados.getValue(Usuario.class);
+
+                    String emailUsuarioAtual = usuarioAtual.getEmail();
+
+                    if (!emailUsuarioAtual.equals(usuario.getEmail())){
+                        listaMembros.add(usuario);
+                    }
+
+                }
+
+             adapterContatos.notifyDataSetChanged();
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
+
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+
+
+        recuperarContatos();
+
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+        //limpar contatos
+        listaMembros.clear();
+        usuariosRef.removeEventListener(valueEventListenerMembros);
     }
 }
